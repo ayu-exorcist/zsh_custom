aa884eb794754c9d22355346c90b5feb147f62a0
ayu <ayu.exorcist@gmail.com>
2025-09-13 16:14:05 +0800

    Remove unused plugin: p10k instant prompt

--- plugins/p10k-instaprompt/p10k-instaprompt.plugin.zsh
+++
@@ -1,7 +0,0 @@
-# Run this early so we can P10k instant prompt if we need it.
-if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
-  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
-fi
-
-# Mark the plugin as loaded
-zstyle ':zsh_custom:plugin:p10k-instaprompt' loaded 'yes'
